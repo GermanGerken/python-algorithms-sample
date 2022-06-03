ca5076c26d06a140472c6eae6fe3a5de7f23dd5d
germangerken <germangerken@yandex.ru>
2022-06-03 16:23:35 +0300

    Knapsack problem Binary heap implementation added

---
+++ Greedy algorithm/Basic/knapsackProblemBinaryHeap.py
@@ -0,0 +1,17 @@
+import heapq  # implementation of the heap queue algorithm standard library modul
+
+
+def heap_knapsack(capacity, items):
+    order = [(-v / w, w) for v, w in items]  # get list of value of weight and weight pairs
+    heapq.heapify(order)  # make binary heap out of order
+
+    acc = 0  # our accumulated value
+    while order and capacity:
+        v_per_w, w = heapq.heappop(order)  # getting elements from heap
+        can_take = min(w, capacity)  # show how much space left in knapsack
+        acc -= v_per_w * w
+        capacity -= can_take
+    return acc
+
+
+print(heap_knapsack(50, [[60, 20], [100, 50], [120, 30]]))
